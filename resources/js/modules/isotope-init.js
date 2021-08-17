4220b4fa8b4b22d40ba2888604537a01c756682a
Colin Lewis <hello@colinlewis.se>
2021-08-17 18:11:22 +0100

    add isotope init, css

--- resources/js/modules/isotope-init.js
+++ resources/js/modules/isotope-init.js
@@ -1,12 +1,41 @@
-var elem = document.querySelector('.isotopeGrid');
+/**
+ * Isotope init
+ */
+
+// Isotope container
+var elem = document.querySelector('.productList');
 var iso = new Isotope( elem, {
-  // options
-  itemSelector: '.isotopeGridItem',
+  // Isotope options
+  itemSelector: '.productCard',
   layoutMode: 'fitRows'
 });
 
-// element argument can be a selector string
-//   for an individual element
-var iso = new Isotope( '.isotopeGrid', {
-  // options
+// Bind the sort button click
+var sortByGroup = document.querySelector('.filtersButtonGroup');
+sortByGroup.addEventListener( 'click', function( event ) {
+  // Only button clicks
+  if ( !matchesSelector( event.target, '.filterButton' ) ) {
+    return;
+  }
+  // Filter by data-filter
+  var filterValue = event.target.getAttribute('data-filter');
+  iso.arrange({ filter: filterValue });
 });
+
+// Change .activeFilter class on buttons
+var buttonGroups = document.querySelectorAll('.buttonGroup');
+for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+  var buttonGroup = buttonGroups[i];
+  radioButtonGroup( buttonGroup );
+}
+
+function radioButtonGroup( buttonGroup ) {
+  buttonGroup.addEventListener( 'click', function( event ) {
+    // Only works with buttons
+    if ( !matchesSelector( event.target, '.filterButton' ) ) {
+      return;
+    }
+    buttonGroup.querySelector('.is-checked').classList.remove('activeFilter');
+    event.target.classList.add('activeFilter');
+  });
+}
